9272a29f8666dc721d2b2bf58f4435ebaddf513b
ht-syko <syko@hyundaiht.co.kr>
2024-01-25 08:35:00 +0900

    기기 제어 데이터 형식으로 수정

--- src/main/java/com/example/tracing4spring3/rsocket/RSocketService.java
+++ src/main/java/com/example/tracing4spring3/rsocket/RSocketService.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,9 +18,32 @@ public class RSocketService {
 
   public Mono<?> rsocketFnF(final String routePath) {
     log.info("rsocket request start");
+
+    Map<String, Object> data = new HashMap<>();
+    data.put("parentDeviceId", "c015898788b3423e8bf127ffc1eb283d");
+    Map<String, String> trigger = new HashMap<>();
+    trigger.put("userId", "testUser");
+    trigger.put("serviceId", "ht-iot-app-ios-v1");
+
+    data.put("trigger", trigger);
+
+
+    ArrayList<Map<String, String>> states = new ArrayList<>();
+    Map<String, String> state1 = new HashMap<>();
+    state1.put("name", "power");
+    state1.put("value", "off");
+
+    Map<String, String> state2 = new HashMap<>();
+    state2.put("name", "setTemperature");
+    state2.put("value", "25");
+    states.add(state1);
+    states.add(state2);
+
+    data.put("states", states);
+
     return this.rSocketRequester
         .route(routePath)
-        .data(Map.of("result", "success"))
+        .data(data)
         .send()
         .then(Mono.just(Map.of("result", "success")));
   }
